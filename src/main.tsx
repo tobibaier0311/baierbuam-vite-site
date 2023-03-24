fa654a76988ff1e503c32a2809fcbe0241542975
tobibaier0311 <tobias.baiier@web.de>
2023-03-24 19:21:59 +0100

    firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
+import { ReactDOM } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const container = document.getElementById('root')
+const container = document.getElementById("root");
 
 if (!container) {
-  throw Error('#root Container not found!')
+  throw Error("#root Container not found!");
 }
 
-const root = createRoot(container)
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
