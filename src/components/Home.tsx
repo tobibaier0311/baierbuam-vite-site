ba091eba5fd51578628358e782ae25f42c3ac259
tobibaier0311 <tobias.baiier@web.de>
2023-03-27 18:39:45 +0200

    About Card

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,31 +1,12 @@
 import { Container, Typography } from "@mui/material";
 import Bild1 from "../assets/bbaf.jpeg";
 import "./Home.css";
+import About from "./home/About";
 
 export function Home() {
   return (
     <Container>
-      <article className="article">
-        <img className="image" src={Bild1} />
-        <h1 className="headline">Rock meets Blasmusik.</h1>
-      </article>
-      <section>
-        <p>
-          Wir, die "Baierbuam And Friends" sind eine Gruppe ambitionierter
-          Hobby-Musiker, welche im Gründungsjahr 2018 noch aus 6 Mitgliedern
-          bestand. Diese Zahl ist bis heute im Jahr 2023 auf ganze 13 gestiegen.
-          Stets mit dem Motto "Rock meets Blasmusik" treten wir auf
-          verschiedenen Veranstaltungen im Raum Passau auf. Von kleineren
-          Auftritten an Geburtstagen bestimmter "Ehrenfans", über musikalische
-          Begleitung verschiedener Dorffeste wie zum Beispiel dem Starkbierfest
-          in Schalding, bis hin zur Unterstützung der 1. Fußballmannschaft des
-          SV Schalding Heining ist alles dabei. Unsere aktuelle Besetzung sind 5
-          Trompeten, 4 Posaunen, 1 Klarinette, 1 E-Gitarre, 1 E-Bass und 1
-          Schlagzeug. Unser Repertoire zeichnet sich durch ein sehr breites
-          Spektrum aus. Enthalten sind natürlich bestimmte All Time Hits wie
-          "Sweet Caroline" oder "Highway to Hell".
-        </p>
-      </section>
+      <About />
     </Container>
   );
 }
---
+++ src/components/home/About.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card, CardContent, CardMedia } from "@mui/material";
+import Cover from "../../assets/bbaf.jpeg";
+
+import "./About.css";
+
+export default function About() {
+  return (
+    <Card className="card" variant="outlined">
+      <CardMedia component="img" image={Cover} sx={{ width: "650px" }} />
+      <CardContent>
+        <h1>Rock meets Blasmusik.</h1>
+        <p>
+          Wir, die "Baierbuam And Friends" sind eine Gruppe ambitionierter
+          Hobby-Musiker, welche im Gründungsjahr 2018 noch aus 6 Mitgliedern
+          bestand. Diese Zahl ist bis heute im Jahr 2023 auf ganze 13 gestiegen.
+          Stets mit dem Motto "Rock meets Blasmusik" treten wir auf
+          verschiedenen Veranstaltungen im Raum Passau auf. Von kleineren
+          Auftritten an Geburtstagen bestimmter "Ehrenfans", über musikalische
+          Begleitung verschiedener Dorffeste wie zum Beispiel dem Starkbierfest
+          in Schalding, bis hin zur Unterstützung der 1. Fußballmannschaft des
+          SV Schalding Heining ist alles dabei. Unsere aktuelle Besetzung sind 5
+          Trompeten, 4 Posaunen, 1 Klarinette, 1 E-Gitarre, 1 E-Bass und 1
+          Schlagzeug. Unser Repertoire zeichnet sich durch ein sehr breites
+          Spektrum aus. Enthalten sind natürlich bestimmte All Time Hits wie
+          "Sweet Caroline" oder "Highway to Hell".
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
