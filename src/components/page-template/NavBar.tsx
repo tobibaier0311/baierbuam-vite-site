167cf24702f5a18d812458b0bcc709ea55cf4be0
tobibaier0311 <tobias.baiier@web.de>
2023-12-10 12:52:28 +0100

    drawer improvements

--- src/components/page-template/NavBar.tsx
+++ src/components/page-template/NavBar.tsx
@@ -1,7 +1,15 @@
-import { Box, Button, Container, Divider, Drawer, IconButton } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Drawer,
+  IconButton,
+  Stack,
+} from "@mui/material";
 import React, { useState } from "react";
 import SiteLinks from "./SiteLinks";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 
 import Logo from "../../assets/Logo.png";
 
@@ -10,42 +18,65 @@ import { Brand } from "../brand/Brand";
 import "./NavBar.css";
 import { useNavigate } from "react-router-dom";
 import { NavItem } from "../nav-item/nav-item";
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
 
 export default function NavBar() {
-  const [open, setOpen] = useState(false)
-  
-  const to = useNavigate(); 
+  const [open, setOpen] = useState(false);
 
-  return ( 
-    <Container>
-      <div className="NavBar">
-        <a onClick={() => to("/")} className="brand">
-          <Brand />
-          <h1 style={{ textDecoration: "none" }}>Baierbuam And Friends</h1>
-        </a>
-        <img src={Logo} width="80px" className="Logo" />
-        <div className="Links">
-          <SiteLinks />
+  const to = useNavigate();
+
+  return (
+    <>
+      <Container>
+        <div className="NavBar">
+          <a onClick={() => to("/")} className="brand">
+            <Brand />
+            <h1 style={{ textDecoration: "none" }}>Baierbuam And Friends</h1>
+          </a>
+          <img src={Logo} width="80px" className="Logo" />
+          <div className="Links">
+            <SiteLinks />
+          </div>
+          <div className="icon">
+            <IconButton onClick={() => setOpen(true)} sx={{ color: "#121212" }}>
+              <MenuIcon fontSize="large" />
+            </IconButton>
+          </div>
         </div>
-        <div className="icon">
-          <IconButton  onClick={() => setOpen(true)} sx={{ color: "#121212"}}>
-          <MenuIcon fontSize="large"/> 
-        </IconButton>
+      </Container>
+      <Drawer open={open} anchor="right" variant="persistent">
+        <div
+          style={{
+            height: "89px",
+            display: "flex",
+            alignItems: "center",
+            padding: "0 1rem",
+          }}
+        >
+          <IconButton edge="end" onClick={() => setOpen(false)}>
+            <ChevronRight />
+          </IconButton>
         </div>
-        
-        <Drawer open={open} anchor="right" variant="temporary" onClose={() => setOpen(false)}>
-          <Box sx={{ width: 150, padding: "0 2rem", display: "flex",flexDirection: "column", alignItems:"start"}}>
-              LOGO?
-              <Divider sx={{ alignSelf: "stretch"}}/>
-              <div onClick={() => setOpen(false)}>
-              <NavItem to="/" text="Home"/>
-              <NavItem to="/gigs" text="Termine"/>
-              <NavItem to="/images" text="Galerie" />
-              <NavItem to="/contact" text="Kontakt"/></div>
-              <Divider sx={{ alignSelf: "stretch"}}/>
-          </Box>
-        </Drawer>
-      </div>
-    </Container>
+        <Box
+          sx={{
+            width: 150,
+            padding: "0 1rem",
+          }}
+        >
+          <Divider sx={{ alignSelf: "stretch" }} />
+          <Stack
+            direction="column"
+            alignItems="start"
+            onClick={() => setOpen(false)}
+          >
+            <NavItem to="/" text="Home" />
+            <NavItem to="/gigs" text="Termine" />
+            <NavItem to="/images" text="Galerie" />
+            <NavItem to="/contact" text="Kontakt" />
+          </Stack>
+          <Divider sx={{ alignSelf: "stretch" }} />
+        </Box>
+      </Drawer>
+    </>
   );
 }
