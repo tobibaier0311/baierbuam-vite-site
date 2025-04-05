08ae66ee932643cb5bee7afef7b564605640de77
tobibaier0311 <tobias.baiier@web.de>
2025-04-05 11:18:36 +0200

    add dultstadl

--- src/data/gigs.ts
+++ src/data/gigs.ts
@@ -37,7 +37,7 @@ export const events: Array<EventsType> = [
     location: "Neustift-Passau",
     description:
       "Rhytmischer Gottesdienst ab 9:15 Uhr in der Kirche Neustift mit Unterstützung unserer kompletten Rhytmusgruppe.",
-    id: 3, 
+    id: 3,
   },
   {
     date: "04. März 2025",
@@ -72,20 +72,26 @@ export const events: Array<EventsType> = [
     id: 7,
   },
   {
+    date: "11. Mai 2025",
+    title: "Muttertag @ Dultstadl auf der Maidult Passau",
+    location: "Maidult Passau",
+    description: "Rockiger Auftritt im Dultstadl am Muttertag, Start um 15 Uhr",
+    id: 8,
+  },
+  {
     date: "17. Mai 2025",
     title: "SV Schalding vs. TSV Nördlingen",
     location: "Sportplatz des SV Schalding am Reuthinger Weg",
     description:
       "Musikalische Unterstützung des Fussballspiels SV Schalding im Spiel gegen den TSV Nördlingen",
-    id: 8,
+    id: 9,
   },
   {
     date: "28. Juni 2025",
     title: "Pfarrfest Heining",
     location: "Heining",
-    description:
-      "Auftritt im Rahmen des Pfarrfestes in Heining",
-    id: 9,
+    description: "Auftritt im Rahmen des Pfarrfestes in Heining",
+    id: 10,
   },
   {
     date: "13. Juli 2025",
@@ -93,15 +99,14 @@ export const events: Array<EventsType> = [
     location: "Sportplatz Hacklberg",
     description:
       "Musikalische Begleitung der Jubiläumsfeier der SpVgg Hacklberg",
-    id: 10,
+    id: 11,
   },
   {
     date: "19. Oktober 2025",
     title: "Kirchweih Schalding r.d.D.",
     location: "Pfarrsaal Schalding",
-    description:
-      "Auftritt im Rahmen der Kirchweih Schalding r.d.D.",
-    id: 11,
+    description: "Auftritt im Rahmen der Kirchweih Schalding r.d.D.",
+    id: 12,
   },
   {
     date: "18. Juli 2026",
@@ -109,6 +114,6 @@ export const events: Array<EventsType> = [
     location: "Schalding l.d.D.",
     description:
       "Musikalische Untermalung der Jubiläumsfeier der Feuerwehr Schalding links der Doanu",
-    id: 12,
+    id: 13,
   },
 ];
