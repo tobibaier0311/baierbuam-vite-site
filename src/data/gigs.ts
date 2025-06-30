2f8f07f97d4deeeb675d56deadb20b2c9106d41f
tobibaier0311 <tobias.baiier@web.de>
2025-06-30 12:47:51 +0200

    Gigs

--- src/data/gigs.ts
+++ src/data/gigs.ts
@@ -16,42 +16,12 @@ export interface EventsType {
 
 export const events: Array<EventsType> = [
   {
-    date: "05. April 2025",
-    title: "SV Schalding-Heining vs. 1860 München II",
-    location: "Sportplatz des SV Schalding am Reuthinger Weg",
+    date: "5. Juli 2025",
+    title: "Sommerfest Langlebenhof Passau",
+    location: "Langlebenhof Passau",
     description:
-      "Musikalische Unterstützung des SV Schalding im Spiel gegen die 2. Mannschaft des TSV 1860 München",
-    id: 6,
-  },
-  {
-    date: "11. Mai 2025",
-    title: "100(+5) Jahre FFW Schalding r.d.D.",
-    location: "Schalding r.d.D.",
-    description:
-      "Musikalische Begleitung der Jubiläumsfeier der Freiwilligen Feuerwehr Schalding rechts der Donau.",
-    id: 7,
-  },
-  {
-    date: "11. Mai 2025",
-    title: "Muttertag @ Dultstadl auf der Maidult Passau",
-    location: "Maidult Passau",
-    description: "Rockiger Auftritt im Dultstadl am Muttertag, Start um 15 Uhr",
-    id: 8,
-  },
-  {
-    date: "17. Mai 2025",
-    title: "SV Schalding vs. TSV Nördlingen",
-    location: "Sportplatz des SV Schalding am Reuthinger Weg",
-    description:
-      "Musikalische Unterstützung des Fussballspiels SV Schalding im Spiel gegen den TSV Nördlingen",
-    id: 9,
-  },
-  {
-    date: "28. Juni 2025",
-    title: "Pfarrfest Heining",
-    location: "Heining",
-    description: "Auftritt im Rahmen des Pfarrfestes in Heining",
-    id: 10,
+      "Musikalische Untermalung des Sommerfestes im Langlebenhof Passau",
+    id: 1,
   },
   {
     date: "13. Juli 2025",
@@ -59,14 +29,27 @@ export const events: Array<EventsType> = [
     location: "Sportplatz Hacklberg",
     description:
       "Musikalische Begleitung der Jubiläumsfeier der SpVgg Hacklberg",
-    id: 11,
+    id: 2,
+  },
+  {
+    date: "20. Juli 2025 ab 14:00 Uhr",
+    title: "Standkonzert Rathausplatz Passau",
+    location: "Rathausplatz Passau",
+    id: 3,
+  },
+  {
+    date: "16. August 2025",
+    title: "Schützenfest Schalding r.d.D.",
+    location: "Schalding r.d.D.",
+    description: "Auftritt im Rahmen des Schützenfestes in Schalding r.d.D.",
+    id: 4,
   },
   {
     date: "19. Oktober 2025",
     title: "Kirchweih Schalding r.d.D.",
     location: "Pfarrsaal Schalding",
     description: "Auftritt im Rahmen der Kirchweih Schalding r.d.D.",
-    id: 12,
+    id: 5,
   },
   {
     date: "18. Juli 2026",
@@ -74,6 +57,6 @@ export const events: Array<EventsType> = [
     location: "Schalding l.d.D.",
     description:
       "Musikalische Untermalung der Jubiläumsfeier der Feuerwehr Schalding links der Doanu",
-    id: 13,
+    id: 6,
   },
 ];
